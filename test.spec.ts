903451b4a62df7a9b64c7be199319f5f49ee4b8a
gurpreet562 <kgurpreet562@gmail.com>
2017-10-30 19:55:36 +0530

    Update test.spec.ts

--- test.spec.ts
+++ test.spec.ts
@@ -12,5 +12,32 @@ export class BannerComponent {
     declarations: [ BannerComponent ],
   })
   .compileComponents(); 
+   describe('CricketerService', () => {
+it('should get cricketetList, done => {
+   let cricketerService: CricketerService;
+getTestBed().compileComponents().then(() => {
+mockBackend.connections.subscribe(
+(connection: MockConnection) => { connection.mockRespond(new Response(
+new ResponseOptions({ body: [
+{ "firstName": "Sachin",lastName": "Tendulakar",
+"favShot": "Drive",playerType": "Batsman",
+"yearlyIncome": "1200000”, dob": "1975-04-10"
+}] }
+)));
+});
+cricketerService = getTestBed().get(CricketerService);
+expect(cricketerService).toBeDefined();
+cricketerService.getCricketerList().subscribe((cricketerList:
+ICricketList[]) => {
+expect(cricketerList.length).toBeDefined();
+expect(cricketerList.length).toEqual(2);
+expect(cricketerList.length).not.toBe(1);
+done();
+});
+});
+});
+});
+});
+});
 }));
 }
